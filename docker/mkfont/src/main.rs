48a01ae5d20db05b5ba68055998adccdaf798603
Robin Grönberg <robin@macbook.lan>
2024-03-24 21:32:21 +0100

    Changes to mkfont to honor index images. Update assets to use indexed images rather than RGB.

--- docker/mkfont/src/main.rs
+++ docker/mkfont/src/main.rs
@@ -3,11 +3,24 @@ use std::{cmp::Ordering, env, fs, path::PathBuf};
 #[derive(Debug)]
 struct ColorCount<const N: usize> {
     count: usize,
+    palette_index: u8,
     color: [u8; N],
 }
 
 #[derive(Debug)]
 struct BitDepthError;
+
+// This function determines the index of a certain color when mapped to the palette.
+// Lower value means closer to index 0 in the palette. In indexed mode, just return the palette. 
+fn color_priority<const N: usize>(c: &ColorCount<N>, color_mode: minipng::ColorType) -> usize {
+    match color_mode {
+        minipng::ColorType::Indexed => {
+            c.palette_index as usize
+        }
+        _ => c.color.iter().map(|i| *i).fold(0, |acc, i| acc + i as usize) * (c.color[3] as usize)
+        // _ => (0 as usize).wrapping_sub(c.count)
+    }
+}
 
 fn get_color(bit_depth: u32, pixels: &[u8], index: usize) -> Result<[u8; 4], BitDepthError> {
     let res = match bit_depth {
@@ -49,9 +62,9 @@ fn do_work() -> i32 {
     };
 
     let pixels = img.pixels();
-
+    let color_type = img.color_type();
     let bit_depth = pixels.len() as u32 / img.width() / img.height() * 8;
-
+    
     // Max 32-bit color
     let mut colors: Vec<ColorCount<4>> = vec![];
 
@@ -63,6 +76,11 @@ fn do_work() -> i32 {
                 return 1;
             };
 
+            let (palette, color) = if color_type == minipng::ColorType::Indexed {
+                let index = color[0];
+                (index, img.palette(index))
+            } else { (0, color) };
+
             if let Some(ref mut color_count) = colors
                 .iter_mut()
                 .find(|color_count| color == color_count.color)
@@ -71,6 +89,7 @@ fn do_work() -> i32 {
             } else {
                 let color_count = ColorCount {
                     color,
+                    palette_index: palette,
                     count: 0,
                 };
                 colors.push(color_count);
@@ -78,10 +97,15 @@ fn do_work() -> i32 {
         }
     }
 
+    if colors.len() > 256 {
+        eprintln!("Incorrect color count. Can handle 256 colors in palette at max. Counted {} different colors", colors.len());
+        return 1;
+    }
+
     colors.sort_by(|left, right| {
-        if left.count < right.count {
+        if color_priority(left, color_type) > color_priority(right, color_type) {
             Ordering::Greater
-        } else if left.count > right.count {
+        } else if color_priority(left, color_type) < color_priority(right, color_type) {
             Ordering::Less
         } else {
             Ordering::Equal
@@ -90,19 +114,26 @@ fn do_work() -> i32 {
 
     eprintln!("Color buckets: {:?}", colors);
 
-    let bpp = match colors.len() {
-        ..=2 => 1,
-        ..=4 => 2,
-        // ..=8 => 3,
-        ..=16 => 4,
-        // ..=32 => 5,
-        // ..=64 => 6,
-        // ..=128 => 7,
-        ..=256 => 8,
-        _ => {
-            eprintln!("Incorrect color count. Can handle 256 colors in palette at max. Counted {} different colors", colors.len());
-            return 1;
-        }
+
+
+    // Assign palette index after sorting if image is non-indexed
+    if color_type != minipng::ColorType::Indexed {
+        colors.iter_mut().enumerate().for_each(|(i, c)| {
+            c.palette_index = i as u8
+        });
+    }
+
+    let max_palette_index = colors.iter().map(|c| c.palette_index).max().unwrap_or(0);
+
+    let bpp = match max_palette_index {
+        ..=1 => 1,
+        ..=3 => 2,
+        // ..=7 => 3,
+        ..=15 => 4,
+        // ..=31 => 5,
+        // ..=63 => 6,
+        // ..=127 => 7,
+        ..=255 => 8,
     };
 
     let mut pixel_colors = vec![0u8; (img.width() * img.height()) as usize];
@@ -125,12 +156,18 @@ fn do_work() -> i32 {
                         return 1;
                     };
 
-                    if let Some((color_index, _)) = colors
+                    let color = if color_type == minipng::ColorType::Indexed {
+                        let index = color[0];
+                        img.palette(index)
+                    } else { color };
+
+                    if let Some((color_index, color_count)) = colors
                         .iter()
                         .enumerate()
                         .find(|(_, color_count)| color == color_count.color)
                     {
                         pixel_colors[i] = color_index as u8;
+                        pixel_colors[i] = color_count.palette_index as u8;
                         i += 1;
                     } else {
                         eprintln!("Could not find color {:?}", color);
@@ -171,11 +208,13 @@ fn do_work() -> i32 {
         .collect::<Vec<_>>();
 
     println!(
-        "img: {}x{}, bit depth: {bit_depth}, uncompressed file size: {} bytes, colors in palette: {}",
+        "Size: {}x{}, Type: {:?}, Bit-Depth: {bit_depth}, Bits per Pixel: {}, Uncompressed Size: {} bytes, Palette Size: {}",
         img.width(),
         img.height(),
+        color_type,
+        bpp,
         pixels.len(),
-        colors.len(),
+        max_palette_index,
     );
 
     let out_path: PathBuf = path.as_str().into();
